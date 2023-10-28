69946f82066862dcd96c1ebfa54434463ce1f218
Tapka3000 <circle.table@yandex.ru>
2023-10-28 19:47:29 +0300

    Commit

--- src/main/java/org/example/Buratinoo.java
+++ src/main/java/org/example/Buratinoo.java
@@ -1,2 +1,14 @@
-package org.example;public class Buratinoo {
+package org.example;
+
+public class Buratinoo {
+    public static void main(String[] args) {
+        int a = 50; // Количество яблок у Буратино
+        int b = 20;// Количество яблок, которые забрал Карабас
+
+        int c = a - b;
+
+        System.out.println("Количество яблок у Буратино" + a);
+        System.out.println("Количество яблок, украдено Карабасом Барабасом" + b);
+        System.out.println(c + " яблок осталось у Буратино");
+    }
 }
