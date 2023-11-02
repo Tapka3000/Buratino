9329fa483a86d30c4d97a3b727d0c898c561d5cb
Tapka3000 <circle.table@yandex.ru>
2023-11-02 10:40:00 +0300

    Commit

---
+++ src/main/java/Buratinoo.java
@@ -0,0 +1,12 @@
+public class Buratinoo {
+    public static void main(String[] args) {
+        int applesPinocchio = 50;
+        int takeKarabas = 20;
+
+        int remainingQuantity = applesPinocchio - takeKarabas;
+
+        System.out.println("Количество яблок у Буратино " + applesPinocchio);
+        System.out.println("Количество яблок, украдено Карабасом Барабасом " + takeKarabas);
+        System.out.println(remainingQuantity + " яблок осталось у Буратино");
+    }
+}
